c96758d005be3bee47cf4a7501bb85005590ef51
Pepinillo2020 <150717538+Pepinillo2020@users.noreply.github.com>
2025-07-08 21:42:32 -0400

    ANIMACIONES EN GENERAL

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -5,6 +5,9 @@ import {
   homeOutline, cameraOutline, chatboxEllipsesOutline, personOutline
 } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { trigger, transition, style, animate, query, group } from '@angular/animations';
+import { Router } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
@@ -13,11 +16,39 @@ import { addIcons } from 'ionicons';
   standalone: true,
   imports: [
     RouterOutlet, RouterLink,
-    IonContent, IonFooter, IonTabBar, IonTabButton, IonIcon, IonLabel
+    IonContent, IonFooter, IonTabBar, IonTabButton, IonIcon, IonLabel,
+    NgClass
+  ],
+  animations: [
+    trigger('routeAnimations', [
+      transition('* <=> *', [
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            width: '100%',
+            top: 0,
+            left: 0,
+          })
+        ], { optional: true }),
+        group([
+          query(':leave', [
+            animate('300ms cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(-100%)', opacity: 0 }))
+          ], { optional: true }),
+          query(':enter', [
+            style({ transform: 'translateX(100%)', opacity: 0 }),
+            animate('300ms cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)', opacity: 1 }))
+          ], { optional: true }),
+        ])
+      ])
+    ])
   ]
 })
 export class MainLayoutComponent {
-  constructor() {
+  constructor(public router: Router) {
     addIcons({ homeOutline, cameraOutline, chatboxEllipsesOutline, personOutline });
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,49 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { IonicRouteStrategy, provideIonicAngular, createAnimation } from '@ionic/angular/standalone';
 import { importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { AuthService } from './app/services/auth.service';
 import { AuthGuard } from './guards/auth.guard';
-import { HttpClientModule } from '@angular/common/http';  // Esto ya lo tienes
+import { HttpClientModule } from '@angular/common/http'; 
+
+
+
+export function slideAnimation(baseEl: any, opts?: any) {
+  const OFF_RIGHT = 'translateX(100%)';
+  const CENTER = 'translateX(0)';
+  const OFF_LEFT = 'translateX(-30%)';
+
+  const enteringEl = baseEl.querySelector(':scope > .ion-page');
+  const leavingEl = baseEl.querySelector(':scope > .ion-page[aria-hidden="true"]');
+
+  const enteringAnimation = createAnimation()
+    .addElement(enteringEl)
+    .beforeStyles({ opacity: 1 })
+    .fromTo('transform', OFF_RIGHT, CENTER)
+    .fromTo('opacity', 0.8, 1);
+
+  const leavingAnimation = createAnimation()
+    .addElement(leavingEl)
+    .fromTo('transform', CENTER, OFF_LEFT)
+    .fromTo('opacity', 1, 0.8);
+
+  return createAnimation()
+    .addAnimation([enteringAnimation, leavingAnimation])
+    .duration(400)
+    .easing('cubic-bezier(0.36,0.66,0.04,1)');
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    provideIonicAngular({
+      navAnimation: slideAnimation
+    }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    importProvidersFrom(HttpClientModule), // 👈 ESTA LÍNEA ES CLAVE
+    importProvidersFrom(HttpClientModule),
     provideAnimations(),
     AuthService,
     AuthGuard,
